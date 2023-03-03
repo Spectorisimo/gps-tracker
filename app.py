362f12bdd95c20bbd0763533252b6427138d0eca
Daniyar Akhmetov <srxd1710@gmail.com>
2023-03-03 23:06:24 +0600

    fixes

--- app.py
+++ app.py
@@ -44,15 +44,15 @@ def decodethis(data):
             record = int(data[19:20], 16)
             timestamp = int(data[20:36], 16)
             priority = int(data[36:38], 16)
-            lon = int(data[38:46], 16)
-            lat = int(data[46:54], 16)
+            lon = str(int(data[38:46], 16))
+            lat = str(int(data[46:54], 16))
             alt = int(data[54:58], 16)
             angle = int(data[58:62], 16)
             sats = int(data[62:64], 16)
             speed = int(data[64:68], 16)
 
             db = DatabaseRepository()
-            timestamp = int(str(timestamp)[:10])
+            timestamp = str(timestamp)[:10]
             date = datetime.datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
             latitude = lat[:2] + '.' + lat[2:]
             longitude = lon[:2] + '.' + lon[2:]
--- src/settings.py
+++ src/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_flvdzp85k6sn%)6q9t)b*8dj0*xh)th4rph62)_b^*#hedp@o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
